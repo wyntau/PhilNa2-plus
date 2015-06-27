8f46406e3fb430750357b4dc4542a9eacc7ada6b
Wyntau <wyntau@outlook.com>
2015-06-27 15:51:31 +0800

    remove all tons copyright

--- templates/notice.php
+++ templates/notice.php
@@ -1,25 +1,6 @@
 <?php
 /**
  * homepage notice
- * display by javascript
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * Required Version:
- * 	PHP5 or higher
- * 	WordPress 2.9 or higher
- *
- * If you find my work useful and you want to encourage the development of more free resources,
- * you can do it by donating...
- * 	paypal: yinheli@gmail.com
- * 	alipay: yinheli@gmail.com
- *
- * @author yinheli <yinheli@gmail.com>
- * @link http://philna.com/
- * @copyright Copyright (C) 2009 yinheli All rights reserved.
- * @license PhilNa2 is released under the GPL.
- * @version $Id$
  */
 
 // no direct access
