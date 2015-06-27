9b6d90902754bdbd9c69b782fe22821c2b7026c3
Wyntau <wyntau@outlook.com>
2015-06-28 00:14:04 +0800

    rename php files

--- comments-popup.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * comments-popup
- */
-
-// no direct access
-defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinheli < http://philna.com/ >');
-
-fail('not available');
