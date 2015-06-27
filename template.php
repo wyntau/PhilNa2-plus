294e87bb88ac82763bdebbe7d93d0dedec1f36c6
Wyntau <wyntau@outlook.com>
2015-06-27 23:28:51 +0800

    makes debug happy

--- template.php
+++ template.php
@@ -16,14 +16,14 @@ get_header();
     <?php wp_nav_menu(array( 'theme_location'=>'primary','container_class' => 'navigation')); ?>
 <div id="top_right">
           <ul id="menu-top_right">
-    <?php if(!$_COOKIE['show_sidebar']=='no'):?>
+    <?php if(isset($_COOKIE['show_sidebar']) && !$_COOKIE['show_sidebar'] == 'no'):?>
       <li id="close-sidebar" title="显示/关闭侧边栏"><a>关闭侧边栏</a></li>
       <li id="show-sidebar" style="display:none;"title="显示/关闭侧边栏"><a>显示侧边栏</a></li>
     <?php else: ?>
       <li id="close-sidebar" style="display:none;" title="显示/关闭侧边栏"><a>关闭侧边栏</a></li>
       <li id="show-sidebar" title="显示/关闭侧边栏"><a>显示侧边栏</a></li>
     <?php endif;?>
-        <?php if($_COOKIE['show_sidebar']=='no'): ?>
+        <?php if(isset($_COOKIE['show_sidebar']) && $_COOKIE['show_sidebar'] == 'no'): ?>
 <style type="text/css">
 #content,#content2{width:920px;}
 #footer {width:920px;}
