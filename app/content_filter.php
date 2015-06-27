f00ef5471283724f7fd3069e62896cec91ea27ee
Wyntau <wyntau@outlook.com>
2015-06-27 22:54:49 +0800

    content_filter

--- app/content_filter.php
+++ app/content_filter.php
@@ -13,17 +13,18 @@ defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinhel
  * @return unknown_type
  */
 function philnacopyright($content){
-  if(is_feed()||is_single()){
+  $after = '';
+  if(is_feed() || is_single()){
     if($GLOBALS['philnaopt']['rss_additional_show'] && $GLOBALS['philnaopt']['rss_additional']){
-    $after= '<div class="copyright_info fontsize12">' . $GLOBALS['philnaopt']['rss_additional'] . '</div>';
-    $blog_link = get_bloginfo('home');
-    $feed_url = get_bloginfo('rss2_url');
-    $post_url = get_permalink();
-    $post_title = get_the_title();
-    $after = str_replace('%BLOG_LINK%', $blog_link, $after);
-    $after = str_replace('%FEED_URL%', $feed_url, $after);
-    $after = str_replace('%POST_URL%', $post_url, $after);
-    $after = str_replace('%POST_TITLE%', $post_title, $after);
+      $after= '<div class="copyright_info fontsize12">' . $GLOBALS['philnaopt']['rss_additional'] . '</div>';
+      $blog_link = get_bloginfo('home');
+      $feed_url = get_bloginfo('rss2_url');
+      $post_url = get_permalink();
+      $post_title = get_the_title();
+      $after = str_replace('%BLOG_LINK%', $blog_link, $after);
+      $after = str_replace('%FEED_URL%', $feed_url, $after);
+      $after = str_replace('%POST_URL%', $post_url, $after);
+      $after = str_replace('%POST_TITLE%', $post_title, $after);
     }
   }
   return $content.$after;
