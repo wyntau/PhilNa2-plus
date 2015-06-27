f9e24ae1939fcf0489ea59dffe3468e9c75c738e
Wyntau <wyntau@outlook.com>
2015-06-27 23:21:20 +0800

    linkpage

--- custom-functions/link-page.php
+++ custom-functions/link-page.php
@@ -4,53 +4,69 @@
  ******************************/
 function my_bookmarks($bookmarks, $args = '' ) {
   $defaults = array(
-    'show_updated' => 0, 'show_description' => 0,
-    'show_images' => 1, 'show_name' => 0,
-    'before' => '<li>', 'after' => '</li>', 'between' => "\n",
-    'show_rating' => 0, 'link_before' => '', 'link_after' => '','nofollow' =>0
+    'show_updated' => 0,
+    'show_description' => 0,
+    'show_images' => 1,
+    'show_name' => 0,
+    'before' => '<li>',
+    'after' => '</li>',
+    'between' => "\n",
+    'show_rating' => 0,
+    'link_before' => '',
+    'link_after' => '',
+    'nofollow' =>0
   );
   $r = wp_parse_args( $args, $defaults );
   extract( $r, EXTR_SKIP );
   $output = ''; // Blank string to start with.
   foreach ( (array) $bookmarks as $bookmark ) {
-    if ( !isset($bookmark->recently_updated) )
+    if ( !isset($bookmark->recently_updated) ){
       $bookmark->recently_updated = false;
+    }
     $output .= $before;
-    if ( $show_updated && $bookmark->recently_updated )
+    if ( $show_updated && $bookmark->recently_updated ){
       $output .= get_option('links_recently_updated_prepend');
+    }
 
     $the_link = '#';
-    if ( !empty($bookmark->link_url) )
+    if ( !empty($bookmark->link_url) ){
       $the_link = clean_url($bookmark->link_url);
+    }
     $rel = ' rel="external';
-    if ($nofollow)
+    if ($nofollow){
       $rel .= ' nofollow';
-    if ( '' != $bookmark->link_rel )
+    }
+    if ( '' != $bookmark->link_rel ){
       $rel .= ' ' . $bookmark->link_rel;
+    }
     $rel .= '"';
     $desc = attribute_escape(sanitize_bookmark_field('link_description', $bookmark->link_description, $bookmark->link_id, 'display'));
     $name = attribute_escape(sanitize_bookmark_field('link_name', $bookmark->link_name, $bookmark->link_id, 'display'));
      $title = $desc;
-    if ( $show_updated )
+    if ( $show_updated ){
       if ( '00' != substr($bookmark->link_updated_f, 0, 2) ) {
         $title .= ' (';
         $title .= sprintf(__('Last updated: %s'), date(get_option('links_updated_date_format'), $bookmark->link_updated_f + (get_option('gmt_offset') * 3600)));
         $title .= ')';
       }
-    if ( '' != $title )
+    }
+    if ( '' != $title ){
       $title = ' title="' . $title . '"';
+    }
     $alt = ' alt="' . $name . '"';
     $target = $bookmark->link_target;
-    if ( '' != $target )
+    if ( '' != $target ){
       $target = ' target="' . $target . '"';
+    }
     $output .= '<a href="' . $the_link . '"' . $rel . $title . $target. '>';
     $output .= $link_before;
     if ( $show_images ) {
       if ( $bookmark->link_image != null) {
-        if ( strpos($bookmark->link_image, 'http') !== false )
+        if ( strpos($bookmark->link_image, 'http') !== false ){
           $output .= "<img src=\"$bookmark->link_image\" $alt $title />";
-        else // If it's a relative path
+        }else{ // If it's a relative path
           $output .= "<img src=\"" . get_option('siteurl') . "$bookmark->link_image\" $alt $title />";
+        }
       } else {//否则显示网站的Favicon
         if (preg_match('/^(https?:\/\/)?([^\/]+)/i',$the_link,$URI)) {//提取域名
           $domains = $URI[2];
@@ -63,10 +79,12 @@ function my_bookmarks($bookmarks, $args = '' ) {
     $output .= $name;
     $output .= $link_after;
     $output .= '</a>';
-    if ( $show_updated && $bookmark->recently_updated )
+    if ( $show_updated && $bookmark->recently_updated ){
       $output .= get_option('links_recently_updated_append');
-    if ( $show_description && '' != $desc )
+    }
+    if ( $show_description && '' != $desc ){
       $output .= $between . $desc;
+    }
     if ($show_rating) {
       $output .= $between . sanitize_bookmark_field('link_rating', $bookmark->link_rating, $bookmark->link_id, 'display');
     }
@@ -76,15 +94,25 @@ function my_bookmarks($bookmarks, $args = '' ) {
 }
 function my_list_bookmarks($args = '') {
   $defaults = array(
-    'orderby' => 'name', 'order' => 'ASC',
-    'limit' => -1, 'category' => '', 'exclude_category' => '',
-    'category_name' => '', 'hide_invisible' => 1,
-    'show_updated' => 0, 'echo' => 1,
-    'categorize' => 1, 'title_li' => __('Bookmarks'),
-    'title_before' => '<h2>', 'title_after' => '</h2>',
-    'category_orderby' => 'name', 'category_order' => 'ASC',
-    'class' => 'linkcat', 'category_before' => '<li id="%id" class="%class">',
-    'category_after' => '</li>','nofollow' => 0
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'limit' => -1,
+    'category' => '',
+    'exclude_category' => '',
+    'category_name' => '',
+    'hide_invisible' => 1,
+    'show_updated' => 0,
+    'echo' => 1,
+    'categorize' => 1,
+    'title_li' => __('Bookmarks'),
+    'title_before' => '<h2>',
+    'title_after' => '</h2>',
+    'category_orderby' => 'name',
+    'category_order' => 'ASC',
+    'class' => 'linkcat',
+    'category_before' => '<li id="%id" class="%class">',
+    'category_after' => '</li>',
+    'nofollow' => 0
   );
   $r = wp_parse_args( $args, $defaults );
   extract( $r, EXTR_SKIP );
@@ -95,8 +123,9 @@ function my_list_bookmarks($args = '') {
     foreach ( (array) $cats as $cat ) {
       $params = array_merge($r, array('category'=>$cat->term_id));
       $bookmarks = get_bookmarks($params);
-      if ( empty($bookmarks) )
+      if ( empty($bookmarks) ){
         continue;
+      }
       $output .= str_replace(array('%id', '%class'), array("linkcat-$cat->term_id", $class), $category_before);
       $catname = apply_filters( "link_category", $cat->name );
       $output .= "$title_before$catname$title_after\n\t<ul class='xoxo blogroll'>\n";
@@ -118,7 +147,8 @@ function my_list_bookmarks($args = '') {
     }
   }
   $output = apply_filters( 'wp_list_bookmarks', $output );
-  if ( !$echo )
+  if ( !$echo ){
     return $output;
+  }
   echo $output;
 }
