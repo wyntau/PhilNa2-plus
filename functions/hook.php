02f78fb04362010df94e80a6de19fb62406cf437
Wyntau <wyntau@outlook.com>
2015-06-28 15:13:00 +0800

    add action to load style

--- functions/hook.php
+++ functions/hook.php
@@ -19,13 +19,14 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+// load jquery
 function philnaLoadJQuery(){
   wp_enqueue_script('jquery-core');
 }
 add_action('wp_enqueue_scripts', 'philnaLoadJQuery');
 
+// load utils function
 function philnaLoadUtil(){
-
   $dev = get_template_directory() . '/js/dev/util.js';
   $min = get_template_directory() . '/js/util.js';
 
@@ -38,10 +39,14 @@ function philnaLoadUtil(){
     $to_load = $dev_uri;
   }
   wp_enqueue_script('philnaUtil', $to_load, array('jquery-core'));
-?>
-<?php
 }
 add_action('wp_enqueue_scripts', 'philnaLoadUtil');
+
+// load styles
+function philnaLoadStyle(){
+  wp_enqueue_style('philnaStyle', get_stylesheet_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'philnaLoadStyle');
 
 //禁用半角符号自动转换为全角
 remove_filter('comment_text', 'wptexturize');
--- templates/header.php
+++ templates/header.php
@@ -7,22 +7,23 @@
 defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinheli < http://philna.com/ >');
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
 <!-- PhilNa2 gorgeous design by by yinheli ( http://philna.com/ ) Remod by 自说Me话(http://ISayMe.com) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <title><?php philnaDocumentTitle(); ?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
-<!--[if lte IE 6]>
-<link media="screen" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/ie6.css" rel="stylesheet">
-<![endif]-->
-<?php if($_COOKIE["comment_author_" . COOKIEHASH]!="") { ?>
-    <script type="text/javascript">document.title = "<?php echo $_COOKIE["comment_author_" . COOKIEHASH].'，欢迎归来~'; ?>" + document.title</script>
-<?php } ?>
-<?php is_404() || wp_head(); 
-if($GLOBALS['philnaopt']['enable_google_analytics']) {
+<?php
+  wp_head();
+  if($_COOKIE["comment_author_" . COOKIEHASH]!="") {
+?>
+  <script type="text/javascript">
+    document.title = "<?php echo $_COOKIE["comment_author_" . COOKIEHASH].'，欢迎归来~'; ?>" + document.title
+  </script>
+<?php
+  }
+  if($GLOBALS['philnaopt']['enable_google_analytics']) {
     if(!$GLOBALS['philnaopt']['exclude_admin_analytics'] || !current_user_can('manage_options')) {
       echo $GLOBALS['philnaopt']['google_analytics_code'];
     }
