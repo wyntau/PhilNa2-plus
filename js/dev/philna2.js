f9e3845a4c233619d0cd018067f48388c5c1d8bc
Wyntau <wyntau@outlook.com>
2015-06-30 00:33:14 +0800

    update stylesheet_uri

--- js/dev/philna2.js
+++ js/dev/philna2.js
@@ -1,4 +1,30 @@
 jQuery(function($) {
+  window.ajax = function(option) {
+    url = option.url ? option.url : blogURL + "?do=ajax";
+    if (option.fn) {
+      url += "&action=" + option.fn
+    }
+    type = option.type ? option.type : "GET";
+    data = option.data ? option.data : null;
+    dataType = option.dataType ? option.dataType : "html";
+    beforeSend = option.beforeSend ? option.beforeSend : null;
+    error = option.error ? option.error : function() {
+      alert(lang.commonError);
+      document.body.style.cursor = "auto"
+    };
+    success = option.success ? option.success : function(w) {
+      alert(w)
+    };
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      dataType: dataType,
+      beforeSend: beforeSend,
+      error: error,
+      success: success
+    });
+  }
 
   function f(v) {
     return v.replace(/\?/g, "---WENHAO---").replace(/&/g, "---ANDHAO---").replace(/=/g, "---DENGHAO---")
