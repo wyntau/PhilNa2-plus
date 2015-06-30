0d04b416de8d154c8948bcee8859899d596d7c04
Wyntau <wyntau@outlook.com>
2015-06-30 15:05:16 +0800

    add clearfix class

--- template.php
+++ template.php
@@ -9,14 +9,12 @@ defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinhel
 get_header();
 ?>
 <div id="wrap">
-  <div id="header" class="box">
+  <div id="header" class="box clearfix">
     <div id="caption" class="icon">
       <?php philnaBlogTitleAndDesc(); ?>
     </div>
     <?php wp_nav_menu(array('theme_location'=>'primary', 'container_class' => 'navigation')); ?>
     <?php philnaCloseSidebar() ?>
-    <?php //wp_page_menu('show_home=1&menu_class=navigation'); ?>
-    <div class="clear"></div>
   </div>
   <?php if(is_single() || is_page()):?>
       <div id="content2">
