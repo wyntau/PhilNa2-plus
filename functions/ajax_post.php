2908bc6148d6ae409b597d902981013fafba2b61
Wyntau <wyntau@outlook.com>
2015-07-02 18:42:09 +0800

    update ajax_post

--- functions/ajax_post.php
+++ functions/ajax_post.php
@@ -1,15 +1,15 @@
 <?php
+
 /********************
  *首页Ajax加载文章关键函数
  ******************/
 function philnaAjaxPost(){
-  if(isset($_GET['action']) && $_GET['action'] == 'philnaAjaxPost' && $_GET['id'] != '') {
+  if(isset($_GET['id']) && $_GET['id'] != '') {
     $ariticle_id=(int)$_GET['id'];
     query_posts("p=$ariticle_id");
-    the_post();
-    the_excerpt();
-    die();
-  }else{
-    return;
+    while (have_posts()){
+      the_post();
+      the_excerpt();
+    }
   }
 }
