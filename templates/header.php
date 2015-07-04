55a0ce217ca0ff9f81cd326daad3a973f09a2f84
Wyntau <wyntau@outlook.com>
2015-07-04 23:40:37 +0800

    format header

--- templates/header.php
+++ templates/header.php
@@ -8,19 +8,19 @@ defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinhel
 
 ?>
 <!DOCTYPE html>
-<!-- PhilNa2 gorgeous design by by yinheli ( http://philna.com/ ) Remod by 自说Me话(http://ISayMe.com) -->
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title><?php philnaDocumentTitle(); ?></title>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta charset="UTF-8">
+  <title><?php philnaDocumentTitle(); ?></title>
 <?php
   wp_head();
   if($_COOKIE["comment_author_" . COOKIEHASH]!="") {
 ?>
-  <script type="text/javascript">
-    document.title = "<?php echo $_COOKIE["comment_author_" . COOKIEHASH].'，欢迎归来~'; ?>" + document.title
-  </script>
+    <script type="text/javascript">
+      document.title = "<?php echo $_COOKIE["comment_author_" . COOKIEHASH].'，欢迎归来~'; ?>" + document.title
+    </script>
 <?php
   }
   if($GLOBALS['philnaopt']['enable_google_analytics']) {
