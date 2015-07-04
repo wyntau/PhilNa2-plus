96a3f5a6b5660dd9dcf3973a9d07f6dcdf70ece7
Wyntau <wyntau@outlook.com>
2015-07-04 22:36:35 +0800

    update

--- archlive_list.php
+++ archlive_list.php
@@ -13,28 +13,28 @@ get_header();
     <?php philnaCloseSidebar() ?>
   </div>
   <div id="content">
-<?php
-if(have_posts()){
-  while(have_posts()){
-    the_post();
-    $postTitleTag = is_singular() ? 'h1' : 'h2';
-?>
-    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <<?php echo $postTitleTag; ?> class="post_title"><a class="icon" href="<?php the_permalink(); ?>" rel="bookmark inlinks permalink" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></<?php echo $postTitleTag; ?>>
-      <div class="postinfo clearfix">
-        <div class="left">
-          <span class="date icon"><?php the_time(__('F jS, Y', YHL)); ?></span>
+    <?php
+    if(have_posts()){
+      while(have_posts()){
+        the_post();
+        $postTitleTag = is_singular() ? 'h1' : 'h2';
+    ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <<?php echo $postTitleTag; ?> class="post_title"><a class="icon" href="<?php the_permalink(); ?>" rel="bookmark inlinks permalink" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></<?php echo $postTitleTag; ?>>
+          <div class="postinfo clearfix">
+            <div class="left">
+              <span class="date icon"><?php the_time(__('F jS, Y', YHL)); ?></span>
+            </div>
+          </div>
+          <div class="post_content content clearfix">
+            <?php the_content(__('Read more...', YHL)); ?>
+            <?php archives_list_SHe(); ?>
+          </div>
         </div>
-      </div>
-      <div class="post_content content clearfix">
-        <?php the_content(__('Read more...', YHL)); ?>
-        <?php archives_list_SHe(); ?>
-      </div>
-    </div>
-<?php
-  } //endwhile;
-} //endif;
-?>
+    <?php
+      } //endwhile;
+    } //endif;
+    ?>
   </div> <!-- #content -->
 <?php
 get_footer();
