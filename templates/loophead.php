3958426634cfc9089d785792bdd89abe463fc20c
Wyntau <wyntau@outlook.com>
2015-07-04 23:41:34 +0800

    format loophead

--- templates/loophead.php
+++ templates/loophead.php
@@ -9,33 +9,34 @@ defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinhel
 ?>
 <div class="loop_head box content">
 <?php
-if(is_search()){
-  echo __('<strong>Search Results : </strong><br/>', YHL),"\n";
-  global $s; printf( __('Keyword: &quot; %1$s &quot;', YHL), wp_specialchars($s, 1) );
-}elseif(is_category()){
-  printf( __('Archive for the &quot;%1$s&quot; Category', YHL), single_cat_title('', false) );
-  if( $desc = category_description()){
-    echo '<p class="desc">', __('Description: ',YHL), '</p>',"\n";
-    echo $desc;
+  if(is_search()){
+    echo __('<strong>Search Results : </strong><br/>', YHL),"\n";
+    global $s;
+    printf( __('Keyword: &quot; %1$s &quot;', YHL), wp_specialchars($s, 1) );
+  }else if(is_category()){
+    printf( __('Archive for the &quot;%1$s&quot; Category', YHL), single_cat_title('', false) );
+    if( $desc = category_description()){
+      echo '<p class="desc">', __('Description: ',YHL), '</p>',"\n";
+      echo $desc;
+    }
+  }else if(is_tag()){
+    printf( __('Posts Tagged &quot;%1$s&quot;', YHL), single_tag_title('', false) );
+    if( $desc = tag_description()){
+      echo '<p class="desc">', __('Description: ',YHL), '</p>',"\n";
+      echo $desc;
+    }
+  }else if(is_day()){
+    printf( __('Archive for %1$s', YHL), get_the_time(__('F jS, Y', YHL)) );
+  }else if (is_month()) {
+    printf( __('Archive for %1$s ', YHL), get_the_time(__('F, Y', YHL)) );
+  }else if(is_year()) {
+    printf( __('Archive for %1$s', YHL), get_the_time(__('Y', YHL)) );
+  }else if(is_author()) {
+    _e('Author Archive', YHL);
+  }else if(isset($_GET['paged']) && !empty($_GET['paged'])) {
+    _e('Blog Archives', YHL);
+  }else{
+    _e('Arhives page', YHL);
   }
-}elseif(is_tag()){
-  printf( __('Posts Tagged &quot;%1$s&quot;', YHL), single_tag_title('', false) );
-  if( $desc = tag_description()){
-    echo '<p class="desc">', __('Description: ',YHL), '</p>',"\n";
-    echo $desc;
-  }
-}elseif(is_day()){
-  printf( __('Archive for %1$s', YHL), get_the_time(__('F jS, Y', YHL)) );
-}elseif (is_month()) {
-  printf( __('Archive for %1$s ', YHL), get_the_time(__('F, Y', YHL)) );
-}elseif(is_year()) {
-  printf( __('Archive for %1$s', YHL), get_the_time(__('Y', YHL)) );
-}elseif(is_author()) {
-  _e('Author Archive', YHL);
-}elseif(isset($_GET['paged']) && !empty($_GET['paged'])) {
-  _e('Blog Archives', YHL);
-}else{
-  _e('Arhives page', YHL);
-}
 ?>
 </div>
