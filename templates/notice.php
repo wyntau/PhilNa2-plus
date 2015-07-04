bbbb3a88123c6695e47a943e97e4ae81f9a62180
Wyntau <wyntau@outlook.com>
2015-07-04 23:46:50 +0800

    format notice

--- templates/notice.php
+++ templates/notice.php
@@ -6,15 +6,16 @@
 // no direct access
 defined('PHILNA') or die('Restricted access -- PhilNa2 gorgeous design by yinheli < http://philna.com/ >');
 
-if(is_home() && $GLOBALS['philnaopt']['notice'] && $GLOBALS['philnaopt']['notice_content']):
-$notice = '<div id="notice" class="box">'.apply_filters('the_content',$GLOBALS['philnaopt']['notice_content']).'</div>';
-$notice = '\''.addslashes($notice).'\'';
-$notice = preg_replace('/\n/','\n', $notice);
+if(is_home() && $GLOBALS['philnaopt']['notice'] && $GLOBALS['philnaopt']['notice_content']){
+  $notice = '<div id="notice" class="box">'.apply_filters('the_content',$GLOBALS['philnaopt']['notice_content']).'</div>';
+  $notice = '\''.addslashes($notice).'\'';
+  $notice = preg_replace('/\n/','\n', $notice);
 ?>
-<script type="text/javascript">
-  /* <![CDATA[ */
-  var PHILNANOTICE = <?php echo $notice; ?>;
-  document.write(PHILNANOTICE);
-  /* ]]> */
-</script>
-<?php endif; ?>
+  <script type="text/javascript">
+    /* <![CDATA[ */
+    var PHILNANOTICE = <?php echo $notice; ?>;
+    document.write(PHILNANOTICE);
+    /* ]]> */
+  </script>
+<?php
+} //endif;
