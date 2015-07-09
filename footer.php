648ca344ff90a54f73d320d22c50d8ed2fabd34a
Wyntau <wyntau@outlook.com>
2015-07-09 23:37:19 +0800

    update footer copyright

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@ is_404() || get_sidebar(); // if 404 page, no sidebar!
     <a id="top" rel="nofollow" href="#header">TOP</a>
     <p>
     <a id="powered" class="icon" title="由WordPress强力驱动" href="http://wordpress.org">WordPress</a>
-    Copyright &copy; 2011-2013 <?php bloginfo('name');?><sup>&reg;</sup><span title="Created On 2011-3-24">&nbsp;&nbsp;本博客已运行<?php echo floor((time()-strtotime("2011-3-24"))/86400); ?>天</span>
+    Copyright &copy; 2011-<?php echo date('Y');?> <?php bloginfo('name');?><sup>&reg;</sup><span title="Created On 2011-3-24">&nbsp;&nbsp;本博客已运行<?php echo floor((time()-strtotime("2011-3-24"))/86400); ?>天</span>
     </p>
     <p id="footerinfo">
       Philna2-plus by <a target="_blank" href="http://ISayMe.com" title="集成功能扩展">自说Me话</a> and PhilNa2 by <a href="http://philna.com">yinheli</a>.
