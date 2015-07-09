d06eaddd676fbcca1a83e8f1b185d747100de5f1
Wyntau <wyntau@outlook.com>
2015-07-09 10:22:13 +0800

    update gravatar_cache

--- hooks/gravatar_cache.php
+++ hooks/gravatar_cache.php
@@ -8,14 +8,17 @@
 add_filter('get_avatar', 'philna_gravatar_cache', 1, 5);
 function philna_gravatar_cache($avatar, $id_or_email, $size = '', $default = '', $alt = ''){
 
+  // 如果不使用缓存, 则直接返回
   if(!(bool)$GLOBALS['philnaopt']['gravatar_cache']){
     return $avatar;
   }
 
+  // 如果不存在, id 或者email, 返回
   if(!$id_or_email){
     return $avatar;
   }
 
+  // 传入的可能是id, 也可能是email
   if(filter_var($id_or_email, FILTER_VALIDATE_EMAIL)){
     $email = $id_or_email;
   }else if( is_numeric( $id_or_email ) ) {
@@ -33,7 +36,7 @@ function philna_gravatar_cache($avatar, $id_or_email, $size = '', $default = '',
       }
     }
   }
-  //
+  // 检查完毕后, 如果email不存在, 则直接返回
   if(!$email){
     return $avatar;
   }
@@ -41,6 +44,7 @@ function philna_gravatar_cache($avatar, $id_or_email, $size = '', $default = '',
   $dir_path = '/wp-content/avatar';
   $dir_path_full = ABSPATH . $dir_path;
 
+  // 如果目录不存在, 则返回
   if(!file_exists($dir_path_full) || !is_writable($dir_path_full)){
     return $avatar;
   }
@@ -56,12 +60,14 @@ function philna_gravatar_cache($avatar, $id_or_email, $size = '', $default = '',
   // 缓存头像7天, 单位秒
   $delta = 24 * 60 * 60 * 7;
 
-  if (!is_file($avatar_path_full) || (time() - filemtime($avatar_path_full)) > $delta){ //當頭像不存在或文件超過14天才更新
+  //當頭像不存在或文件超過7天才更新
+  if (!is_file($avatar_path_full) || (time() - filemtime($avatar_path_full)) > $delta){
 
     $gravatar_block = $dir_path_full . '/.gravatar_block';
     // 每隔一天检查一次gravatar服务是否可用, 单位秒
     $gravatar_block_check_interval = 24 * 60 * 60 * 1;
-    // have .gravatar_block file, and is created rencently, so the server is not available
+    // have .gravatar_block file, and is created rencently
+    // so the Gravatar service is not available in this wordpress server
     if(is_file($gravatar_block) && (time() - filemtime($gravatar_block) <= $gravatar_block_check_interval)){
       return $avatar;
     }
